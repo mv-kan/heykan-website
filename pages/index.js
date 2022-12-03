f0b1c86e984d616fedd203f886d080a5cfb1a9fe
Mykhailo Kan <michael.kan.mail@gmail.com>
2022-12-03 23:02:06 +0100

    blog list

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,45 @@ import matter from 'gray-matter'
 import { MDXRemote } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
 import path from 'path'
-import { POSTS_PATH } from '../utils/mdxUtils'
+import { postFilePaths, POSTS_PATH } from '../utils/mdxUtils'
 import Layout from '../components/layout';
 
-export default function Home({ source, frontMatter }) {
+export default function Home({ source, frontMatter, postsMeta }) {
   return (
     <Layout home> 
       <main>
         <MDXRemote {...source} />
         <div>
-          
+          {
+            postsMeta.map(meta => 
+              <div>
+                <div>{meta.title}</div>
+                <div>{meta.description}</div>
+              </div>
+              )
+          }
         </div>
       </main>
     </Layout>
   )
+}
+
+const getPostMeta = async () => {
+  const allPostMeta = []
+  for (const postFile of postFilePaths) {
+    const postFilePath = path.join(POSTS_PATH, postFile)
+    console.log(postFilePath)
+
+    if (postFilePath === path.join(POSTS_PATH, `index.mdx`)) {
+      continue;
+    }
+    const source = fs.readFileSync(postFilePath)
+  
+    const { content, data } = matter(source)
+    allPostMeta.push(data)
+  }
+  console.log(allPostMeta)
+  return allPostMeta;
 }
 
 export const getStaticProps = async ({ params }) => {
@@ -34,10 +59,12 @@ export const getStaticProps = async ({ params }) => {
     scope: data,
   })
 
+  const postsMetadata = await getPostMeta()
   return {
     props: {
       source: mdxSource,
       frontMatter: data,
+      postsMeta: postsMetadata,
     },
   }
 }
