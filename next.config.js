eff82e306399e73553a3356eb86fffdcef544c6e
Mykhailo Kan <michael.kan.mail@gmail.com>
2022-12-05 14:23:26 +0100

    dockerfile

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['heykan-images-213453.s3.eu-central-1.amazonaws.com'],
-  }
+  },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
