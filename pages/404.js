027ef051ab0f6a247e4ba1eb54b07dd580888b61
Mykhailo Kan <michael.kan.mail@gmail.com>
2022-12-07 13:06:07 +0100

    add 404

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Layout from '../components/layout';
+export default function Document() {
+  return (
+    <Layout>
+        <main>
+            404 - <div className='inline-block text-2xl font-bold'>Nothing cool here!</div> get out!
+        </main>
+    </Layout> 
+  )
+}
